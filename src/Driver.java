a59eac68f30a6ed81a0e0d2d169f401e6140674f
Abdel <mokbe004@umn.edu>
2022-12-29 01:16:09 -0600

    tested on MNIST dataset

--- src/Driver.java
+++ src/Driver.java
@@ -1,15 +1,70 @@
+import java.io.*;
+
 public class Driver {
-  public static void main(String[] args) {
-    Matrix m1 = new Matrix(new double[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-    Matrix m2 = new Matrix(new double[][]{{3},{3},{3}});
-    Network n = new Network(new int[]{5, 16, 16, 10, 5}, new SigmoidFunction());
-    n.eta = 0.1;
-    Matrix input = new Matrix(new double[][]{{0},{0},{0},{0},{0}});
-    Matrix expected = new Matrix(new double[][]{{0.25},{0.8},{0.5},{0.6},{0.75}});
-    for(int i = 0; i < 100; i++) {
-      System.out.println(n.getResult(input));
-      n.train(input, expected);
+  public static void main(String[] args) throws IOException {
+
+    var s = new DataInputStream(new BufferedInputStream(new FileInputStream("data/train-images.idx3-ubyte")));
+    s.readInt();
+    int n = s.readInt();
+    int R = s.readInt();
+    int C = s.readInt();
+    Matrix[] trainingData = new Matrix[n];
+    for(int i = 0; i < n; i++) {
+      trainingData[i] = new Matrix(R, C);
+      for(int r = 0; r < R; r++) for(int c = 0; c < C; c++) trainingData[i].mat[r][c] = s.readUnsignedByte();
     }
-    System.out.println(n.getResult(input));
+    s.close();
+
+    s = new DataInputStream(new BufferedInputStream(new FileInputStream("data/train-labels.idx1-ubyte")));
+    s.readInt(); s.readInt();
+    int[] trainingLabels = new int[n];
+    for(int i = 0; i < n; i++) trainingLabels[i] = s.readUnsignedByte();
+    s.close();
+
+    s = new DataInputStream(new BufferedInputStream(new FileInputStream("data/t10k-images.idx3-ubyte")));
+    s.readInt();
+    n = s.readInt();
+    s.readInt(); s.readInt();
+    Matrix[] testData = new Matrix[n];
+    for(int i = 0; i < n; i++) {
+      testData[i] = new Matrix(R, C);
+      for(int r = 0; r < R; r++) for(int c = 0; c < C; c++) testData[i].mat[r][c] = s.readUnsignedByte();
+    }
+    s.close();
+
+    s = new DataInputStream(new BufferedInputStream(new FileInputStream("data/t10k-labels.idx1-ubyte")));
+    s.readInt(); s.readInt();
+    int[] testLabels = new int[n];
+    for(int i = 0; i < n; i++) testLabels[i] = s.readUnsignedByte();
+    s.close();
+
+    Network N = new Network(new int[]{R*C, 100, 10}, new SigmoidFunction());
+    for(int e = 0; e < 1; e++) for(int i = 0; i < trainingData.length; i++) N.train(transformImage(trainingData[i]), tranformLabel(trainingLabels[i]));
+    int correct = 0; int tested = 0;
+    for(int i = 0; i < 100; i++) {
+      Matrix r = N.getResult(transformImage(testData[i]));
+      int l = 0; double m = r.mat[0][0];
+      for(int j = 1; j < 10; j++) if(r.mat[j][0] > m) {
+        m = r.mat[j][0];
+        l = j;
+      }
+      System.out.printf("expected: %d,  predicted: %d\n", testLabels[i], l);
+      if(l == testLabels[i]) correct++;
+      tested++;
+    }
+    double acc = ((double)correct/tested)*100;
+    System.out.printf("accuracy: %.2f%%\n", acc);
+  }
+
+  static Matrix transformImage(Matrix m) {
+    Matrix r = new Matrix(m.rows*m.cols, 1);
+    for(int i = 0; i < m.rows; i++) for(int j = 0; j < m.cols; j++) r.mat[i*m.cols + j][0] = m.mat[i][j]/255;
+    return r;
+  }
+
+  static Matrix tranformLabel(int l) {
+    Matrix r = new Matrix(10, 1);
+    r.mat[l][0] = 1;
+    return r;
   }
 }
